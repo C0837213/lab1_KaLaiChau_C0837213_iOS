b698c3ba5a9efd4ba852f9ced4f1a1788aae9679
Philip Chau <kyoc2020@gmail.com>
2022-01-18 19:48:47 -0500

    Initial Commit

---
+++ lab1_KaLaiChau_C0837213_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab1_KaLaiChau_C0837213_iOS
+//
+//  Created by Philip Chau on 18/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
